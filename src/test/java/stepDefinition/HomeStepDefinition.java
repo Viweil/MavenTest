56a2a252c07a4587f8fff679b159da95683b873b
Viweil <moccababy777@gmail.com>
2017-08-21 12:20:04 +1200

    modify dns

--- src/test/java/stepDefinition/HomeStepDefinition.java
+++ src/test/java/stepDefinition/HomeStepDefinition.java
@@ -16,12 +16,12 @@ public class HomeStepDefinition {
 	public void i_go_to(String arg1) {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.navigate().to("http://ec2-35-176-90-7.eu-west-2.compute.amazonaws.com/CrazyHatProject/");
+		driver.navigate().to("http://ec2-35-176-202-196.eu-west-2.compute.amazonaws.com/CrazyHatProject/");
 	}
 
 	@Then("^I should be on \"([^\"]*)\" Home Page$")
 	public void i_should_be_on_Home_Page(String arg1) {
-		Assert.assertTrue(driver.getTitle().contains("CRAZY HATS"));
+		Assert.assertTrue(driver.getTitle().contains("CRAZY HATS")); 
 		driver.close();
 	}
 	
