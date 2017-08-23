076f305db3edc18f8d16167e510c58256ec93bbc
Viweil <moccababy777@gmail.com>
2017-08-24 10:17:45 +1200

    remove an import

--- src/test/java/stepDefinition/HomeStepDefinition.java
+++ src/test/java/stepDefinition/HomeStepDefinition.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
